64b5d246fae14ef4f5b2aa1449a875f4e19dda5f
AgustinGurvich <agustingurvich@hotmail.com>
2018-11-30 16:54:06 -0300

    Add files via upload
    Funciona todo, crear lectura de archivo

--- laberinto.py
+++ laberinto.py
@@ -1,26 +1,31 @@
 def buscarSalida(laberinto,x,y,listapos):
-    if laberinto[x][y]==1: #encontre una pared
-        return False
-    elif laberinto[x][y] == 2: #encontre la salida
-        listapos.append((True,x,y))
-        return listapos
+    print(x,y)
+    if laberinto[x][y]==1:
+        return (False,x)
+    elif laberinto[x][y] == 2:
+        listapos.append((x,y))
+        tupla = True
+        return tupla
     elif laberinto[x][y]==3: #encontre una ubicacion que ya visite
         return False
-    laberinto[x][y]==3 #marco la ubicacion como visitada
-    a = b = c = d = (False,x,y)
-    if x < len(laberinto):
-        a=buscarSalida(laberinto,x+1,y,listapos)
-    if y < len(laberinto[0]):
-        b=buscarSalida(laberinto,x,y+1,listapos)
+    laberinto[x][y]=3 #marco la ubicacion como visitada
+    a=b=c=d=False
+    if x < len(laberinto)-1:
+        a = buscarSalida(laberinto,x+1,y,listapos)
+    if y < len(laberinto[x])-1:
+        b = buscarSalida(laberinto,x,y+1,listapos)
     if  x > 0:
-        c=buscarSalida(laberinto,x-1,y,listapos)
+        c = buscarSalida(laberinto,x-1,y,listapos)
     if y > 0:
-        d=buscarSalida(laberinto,x,y-1,listapos)
-    if (a[0] or b[0] or c[0] or d[0]):
-        return listapos.append((True,x,y))
+        d = buscarSalida(laberinto,x,y-1,listapos)
+    if (a or b or c or d):
+        listapos.append((x,y))
+        tupla = True
+        return tupla
     return False
 def main():
     posiciones=[]
-    labprueba=[[0,1,1,1],[0,1,1,1],[2,1,1,1]]
-    buscarSalida(labprueba,0,0,posiciones)
+    labprueba=[[0,1,1,1],[0,0,1,1],[1,0,0,2]]
+    resolucion = buscarSalida(labprueba,0,0,posiciones)
+    print(posiciones)
 main()
